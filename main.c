744619ef3c4547242d3a382ae5140392c0248f75
Manish <manish.adhikari2402@gmail.com>
2025-07-27 19:10:41 +0530

    Added string parser

--- main.c
+++ main.c
@@ -9,6 +9,7 @@ int parse_int(char *str);
 int main()
 {
     char *str = "i42e";
+    char* byte_string = "44:hello";
     return 0;
 }
 
@@ -38,6 +39,29 @@ int parse_int(char *str)
     return 0;
 }
 
-// int parse_bytes(char *str) {
-    
-// }
+int parse_bytes(char *str)
+{
+    char *pos = strchr(str, ':');
+    if (pos == NULL)
+    {
+        fprintf(stderr, "Error: Invalid format\n");
+        return -1;
+    }
+    size_t temp_len = pos - str;
+    char temp_str[pos - str + 1];
+    strncpy(temp_str, str, temp_len);
+    temp_str[temp_len] = '\0';
+    int byte_len = atoi(temp_str);
+    if (byte_len == 0)
+    {
+        fprintf(stderr, "Error: Invalid format\n");
+        return -1;
+    }
+
+    char *new_str = pos + 1;
+    for (size_t i = 0; i < byte_len; i++)
+    {
+        printf("%c", new_str[i]);
+    }
+    return 0;
+}
